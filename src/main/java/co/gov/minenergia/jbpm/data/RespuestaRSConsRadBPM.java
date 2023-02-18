8c272930937a3302e7f7c4a19c2cea0556d7cbb4
wbadmin <>
2023-02-17 19:07:21 -0500

    {/src/main/java/co/gov/minenergia/jbpm/data/RespuestaRSConsRadBPM.java}

--- src/main/java/co/gov/minenergia/jbpm/data/RespuestaRSConsRadBPM.java
+++ src/main/java/co/gov/minenergia/jbpm/data/RespuestaRSConsRadBPM.java
@@ -6,12 +6,58 @@ package co.gov.minenergia.jbpm.data;
 
 public class RespuestaRSConsRadBPM implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public RespuestaRSConsRadBPM() {
-    }
+	private co.gov.minenergia.jbpm.data.RegistroConsultaRadicado registro;
+	private java.util.List<co.gov.minenergia.jbpm.data.DatosTipificacion> trd;
+	private java.lang.String expediente;
+	private java.lang.String message;
 
+	public RespuestaRSConsRadBPM() {
+	}
 
+	public co.gov.minenergia.jbpm.data.RegistroConsultaRadicado getRegistro() {
+		return this.registro;
+	}
 
+	public void setRegistro(
+			co.gov.minenergia.jbpm.data.RegistroConsultaRadicado registro) {
+		this.registro = registro;
+	}
+
+	public java.util.List<co.gov.minenergia.jbpm.data.DatosTipificacion> getTrd() {
+		return this.trd;
+	}
+
+	public void setTrd(
+			java.util.List<co.gov.minenergia.jbpm.data.DatosTipificacion> trd) {
+		this.trd = trd;
+	}
+
+	public java.lang.String getExpediente() {
+		return this.expediente;
+	}
+
+	public void setExpediente(java.lang.String expediente) {
+		this.expediente = expediente;
+	}
+
+	public java.lang.String getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(java.lang.String message) {
+		this.message = message;
+	}
+
+	public RespuestaRSConsRadBPM(
+			co.gov.minenergia.jbpm.data.RegistroConsultaRadicado registro,
+			java.util.List<co.gov.minenergia.jbpm.data.DatosTipificacion> trd,
+			java.lang.String expediente, java.lang.String message) {
+		this.registro = registro;
+		this.trd = trd;
+		this.expediente = expediente;
+		this.message = message;
+	}
 
 }
